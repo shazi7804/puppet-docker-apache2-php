adb287cb9b7fa59b5b09b0fc8c1102143f324db6
scott.liao <shazi7804@gmail.com>
2018-01-02 03:27:45 +0800

    fix test

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
-dpkg -i puppet5-release-xenial.deb
-apt update && apt install puppet-agent -y
+sudo dpkg -i puppet5-release-xenial.deb
+sudo apt update && sudo apt install puppet-agent -y
